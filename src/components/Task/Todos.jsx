755eee8acf30f4641fd248874567a49365451cdc
thenerdone <poria9chegeni@gmail.com>
2021-02-18 12:41:15 +0330

    final commit after simple routing

--- src/components/Task/Todos.jsx
+++ src/components/Task/Todos.jsx
@@ -1,31 +1,38 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import Todo from "./Todo";
 import TodoContext from "../../context/TodoContext";
+import AddNewTodo from "./AddNewTodo";
+
 class Todos extends Component {
   render() {
     return (
-      <TodoContext.Consumer>
-        {(context) =>
-          context.state.todos.length > 0 ? (
-            context.state.todos.map((todo) => (
-              <li key={context.state.todo.id} className="list-group-item">
-                <Todo
-                  text={todo.text}
-                  isCompleted={todo.completed}
-                  deleted={() => context.handleDeleteTodo(todo.id)}
-                  completed={() => context.handleCompletedTodo(todo.id)}
-                />
-              </li>
-            ))
-          ) : (
-            <div className="alert alert-light mt-3 w-75 mx-auto">
-              <p className="text-center">
-                هیچ کاری برای امروز ثبت نشده دوست من
-              </p>
-            </div>
-          )
-        }
-      </TodoContext.Consumer>
+      <Fragment>
+        <TodoContext.Consumer>
+          {(context) =>
+            context.state.todos.length > 0 ? (
+              context.state.todos.map((todo) => (
+                <li className="list-group-item" key={todo.id}>
+                  <Todo
+                    text={todo.text}
+                    isCompleted={todo.completed}
+                    deleted={() => context.handleDeleteTodo(todo.id)}
+                    completed={() => context.handleCompletedTodo(todo.id)}
+                    key={todo.id}
+                    id={todo.id}
+                  />
+                </li>
+              ))
+            ) : (
+              <div className="alert alert-light mt-3 w-75 mx-auto">
+                <p className="text-center">
+                  هیچ کاری برای امروز ثبت نشده دوست من
+                </p>
+              </div>
+            )
+          }
+        </TodoContext.Consumer>
+        <AddNewTodo />
+      </Fragment>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom";
+import GlobalState from "./context/GlobalState";
 import App from "./containers/App";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDom.render(<App />, document.getElementById("root"));
+ReactDom.render(
+  <GlobalState>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </GlobalState>,
+  document.getElementById("root")
+);
